3a594cc86654943329f62b6217d2dfba222c546a
Newky <richdel1991@gmail.com>
2013-06-26 22:42:33 +0100

    Made spawn into an actual module that is hosted on pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spawn',
+      version='0.1',
+      description='Python module to spawn $EDITOR for user input in a temporary file. (think git commit without the -m) ',
+      url='http://github.com/Newky/spawn_editor',
+      author='Richard Delaney',
+      author_email='richdel1991@gmail.com',
+      license='MIT',
+      packages=['spawn'],
+      zip_safe=False)
---
+++ spawn/__init__.py
@@ -0,0 +1,1 @@
+from .spawn import get_editor_output
